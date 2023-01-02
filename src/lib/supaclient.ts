2c4f09c8e5ac720570e906ec787772ada4add2bc
0hundred0 <kilgamesh@antivillain.io>
2023-01-02 02:06:04 -0500

    Fix: changed supabase url

--- src/lib/supaclient.ts
+++ src/lib/supaclient.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://anerznxazdnmkryaywbn.supabase.co';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supakey = process.env.SUPABASE_KEY;
 
+if(!supabaseUrl) throw console.error('Missing supabase url.');
 export const supabase = createClient(supabaseUrl, supakey);
