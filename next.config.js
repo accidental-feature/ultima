0076773f126c69e50fc0d358f96c63ed48516406
kilgamesh <notkilgamesh@gmail.com>
2023-05-15 15:58:57 -0400

    fix: update API server to supabase

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   swcMinify: true,
   env: {
     EMAIL_PUBLIC_KEY: process.env.EMAIL_PUBLIC_KEY,
 		EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
 		EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID
+  },
+	async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      },
+    ];
   },
 }
 
