34a3d41661531872de5b5b01e065b26288bcb74d
kilgamesh <notkilgamesh@gmail.com>
2023-05-15 15:32:30 -0400

    test: Add vercel.json for CORs get

--- next.config.js
+++ next.config.js
@@ -7,18 +7,6 @@ const nextConfig = {
 		EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
 		EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID
   },
-	async headers() {
-		return [
-		{
-			source: "/api/(.*)",
-			headers: [
-				{ key: "Access-Control-Allow-Credentials", value: "true" },
-				{ key: "Access-Control-Allow-Origin", value: "ultima-hvlxqaodh-kilgamesh.vercel.app" },
-				{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-				{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-			]
-		}]
-	},
 }
 
 module.exports = nextConfig
