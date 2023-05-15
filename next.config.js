59f797eba3f040c4ec8906eee9b7ac53be85e98e
kilgamesh <notkilgamesh@gmail.com>
2023-05-15 15:17:37 -0400

    testing:Cors Error

--- next.config.js
+++ next.config.js
@@ -4,9 +4,21 @@ const nextConfig = {
   swcMinify: true,
   env: {
     EMAIL_PUBLIC_KEY: process.env.EMAIL_PUBLIC_KEY,
-	EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
-	EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID
+		EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
+		EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID
   },
+	async headers() {
+		return [
+		{
+			source: "/api/(.*)",
+			headers: [
+				{ key: "Access-Control-Allow-Credentials", value: "true" },
+				{ key: "Access-Control-Allow-Origin", value: "https://www.ultima.rest/" },
+				{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+				{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+			]
+		}]
+	},
 }
 
 module.exports = nextConfig
