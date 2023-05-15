d412eb6566524c18e8062de406a8168f18b62562
KJ <notkilgamesh@gmail.com>
2023-05-15 15:42:04 -0400

    testing:Cors Error (#50)
    * testing:Cors Error
    
    * chore: temp link in next.config
    
    * test: Add vercel.json for CORs get
    
    * fix: Cors issue

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   swcMinify: true,
   env: {
     EMAIL_PUBLIC_KEY: process.env.EMAIL_PUBLIC_KEY,
-	EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
-	EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID
+		EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
+		EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID
   },
 }
 
