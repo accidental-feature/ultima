2b3a76261fbb3de5b69ad50cbc5c3f21edb50299
KJ <notkilgamesh@gmail.com>
2023-05-15 21:36:38 -0400

    chore: Update next.config.js
    Added CORs headers

--- next.config.js
+++ next.config.js
@@ -3,9 +3,22 @@ const nextConfig = {
   swcMinify: true,
   env: {
     EMAIL_PUBLIC_KEY: process.env.EMAIL_PUBLIC_KEY,
-		EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
-		EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID
+    EMAIL_SERVICE_ID: process.env.EMAIL_SERVICE_ID,
+    EMAIL_TEMPLATE_ID: process.env.EMAIL_TEMPLATE_ID
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
